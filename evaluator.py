bda014a31957c2151eccf797b60a6e5d7e2b4fa5
mhimavamshi <himavamshi1212@gmail.com>
2026-07-12 22:42:36 +0530

    evaluator skeleton is done

---
+++ evaluator.py
@@ -0,0 +1,35 @@
+from parser import NodeType
+
+class Evaluator:
+    def __init__(self, strategy):
+        self.strategy = strategy
+        self.reductions = 0
+
+    def evaluate(self, tree):
+        if tree.nodetype == NodeType.APPLICATION:
+            if tree.left.nodetype == NodeType.LAMBDA:
+                self.reductions += 1
+                return self.substitute(tree.left, tree.right)
+            
+            tree.left = self.evaluate(tree.left)
+            tree.right =self.evaluate(tree.right)
+
+            return tree
+
+        elif tree.nodetype == NodeType.LAMBDA:
+            if self.strategy == "strong":
+                tree.right = self.evaluate(tree.right)
+            return tree
+
+        elif tree.nodetype == NodeType.VARIABLE:
+            return tree
+
+        else:
+            return tree
+
+    def reduce(self, tree):
+        while True:
+            self.reductions = 0
+            tree = self.evaluate(tree)
+            if self.reductions == 0:
+                return tree  
