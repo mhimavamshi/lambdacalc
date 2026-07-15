43fa7924bb3bafe379010368914fe8f950f7b70d
mhimavamshi <himavamshi1212@gmail.com>
2026-07-15 23:14:09 +0530

    add beta reduction and substitution

--- evaluator.py
+++ evaluator.py
@@ -1,4 +1,4 @@
-from parser import NodeType
+from parser import NodeType, ApplicationNode, LambdaNode
 from utils import debug_print
 
 class Evaluator:
@@ -6,14 +6,39 @@ class Evaluator:
         self.strategy = strategy
         self.reductions = 0
 
+    def alpha_convert(self, tree):
+        # it goes through the tree
+        # keeps track of variables in outer scope
+        # or scopes
+        # if its newly defined again
+        # we can have metadata saying they're new ones bound to new lambda, don't substitute
+        # or just encode that information in the re-naming
+        pass
+
+    def substitute(self, node, replacement, variable):
+        # lambda is simply a variable: \x.x 
+        if node.nodetype == NodeType.VARIABLE: 
+            return replacement if node.value == variable else node 
+
+        # of course, alpha_convert is necessary
+        # ignore, for now
+        if node.nodetype == NodeType.LAMBDA:       
+            body = node.right
+            return LambdaNode(node.value, self.substitute(body, replacement, variable))
+            
+        if node.nodetype == NodeType.APPLICATION:
+            return ApplicationNode(self.substitute(node.left, replacement, variable), self.substitute(node.right, replacement, variable))
+        
+
     def evaluate(self, tree):
         if tree.nodetype == NodeType.APPLICATION:
             if tree.left.nodetype == NodeType.LAMBDA:
                 self.reductions += 1
-                return self.substitute(tree.left, tree.right)
+                body = tree.left.right
+                return self.substitute(body, tree.right, tree.left.value)
             
             tree.left = self.evaluate(tree.left)
-            tree.right =self.evaluate(tree.right)
+            tree.right = self.evaluate(tree.right)
 
             return tree
 
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import tokenizer
 from parser import Parser, print_tree
+from evaluator import Evaluator
 
 
 def test_expression(name, source):
@@ -14,60 +15,37 @@ def test_expression(name, source):
     for token in tokens:
         print(token)
 
-    print("\n=== AST ===")
     parser = Parser(tokens)
     tree = parser.parse()
+
+    print("\n=== Parsed AST ===")
     print_tree(tree)
+
+    evaluator = Evaluator(strategy="weak")
+    reduced = evaluator.reduce(tree)
+
+    print("\n=== Reduced AST ===")
+    print_tree(reduced)
 
     print()
 
-
-def main():
+def test_expressions():
     tests = [
-        (
-            "identity",
-            r"\x.x"
-        ),
-        (
-            "simple application",
-            r"(\x.x) y"
-        ),
-        (
-            "nested lambda",
-            r"\x.\y.x"
-        ),
-        (
-            "application chain",
-            r"a b c"
-        ),
-        (
-            "lambda body application",
-            r"\x.x y"
-        ),
-        (
-            "nested application",
-            r"(\x.x) ((\y.y) z)"
-        ),
-        (
-            "lambda as argument",
-            r"(\x.x) (\y.y)"
-        ),
-        (
-            "complex",
-            r"(\f.\x.f (f x)) (\y.y) a"
-        ),
-        (
-            "very complex",
-            r"(\a.\b.a (\c.c b)) ((\x.x) (\y.y)) z"
-        ),
-        (
-            "something ive seen",
-            r"\m.\n.\f.\x.m f (n f x)"
-        )
+        ("identity", r"(\x.x) y"),                    # -> y
+        ("identity application", r"(\x.x) (a b)"),   # -> (a b)
+        ("constant", r"(\x.\y.x) a b"),              # -> a
+        ("second", r"(\x.\y.y) a b"),                # -> b
+        ("duplicate", r"(\x.x x) y"),                # -> y y
+        ("ignore arg", r"(\x.a) b"),                 # -> a
+        ("nested lambda", r"(\x.\y.x) y"),                 # -> \y.y 
     ]
 
     for name, source in tests:
         test_expression(name, source)
+
+
+def main():
+    test_expressions()
 
 
 if __name__ == "__main__":
